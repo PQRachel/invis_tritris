fbd8d641f70829297e7e93c3322797b0caa6348f
PQRachel <88564179+PQRachel@users.noreply.github.com>
2022-01-03 18:23:46 -0500

    Update game.js

--- game.js
+++ game.js
@@ -504,7 +504,7 @@ class Game {
         const cellW = w / this.w;
         const cellH = h / this.h;
 
-        this.grid.show(x, y, w, h, this.colors, this.pieceImages, paused, showGridLines, oldGraphics);
+        this.grid.show(x, y, w, h, this.pieceImages, paused, showGridLines, oldGraphics);
         if (this.currentPiece && !paused) {
             this.currentPiece.show(x, y, cellW, cellH, this.colors, this.pieceImages, oldGraphics);
         }
